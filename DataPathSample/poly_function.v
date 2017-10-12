76b8e14c51e1223541fe50c40caa80ef5a345ec1
fhboswell <franklin.henry.boswell@gmail.com>
2017-10-11 17:10:03 -0700

    working on ram sample

--- DataPathSample/poly_function.v
+++ DataPathSample/poly_function.v
@@ -108,7 +108,11 @@ module control(
                 S_LOAD_X_WAIT   = 4'd7,
                 S_CYCLE_0       = 4'd8,
                 S_CYCLE_1       = 4'd9,
-                S_CYCLE_2       = 4'd10;
+                S_CYCLE_2       = 4'd10,
+					 S_CYCLE_3       = 4'd11,
+					 S_CYCLE_4       = 4'd12,
+					 S_CYCLE_5       = 4'd13;
+					
     
     // Next state logic aka our state table
     always@(*)
@@ -123,7 +127,11 @@ module control(
                 S_LOAD_X: next_state = go ? S_LOAD_X_WAIT : S_LOAD_X; // Loop in current state until value is input
                 S_LOAD_X_WAIT: next_state = go ? S_LOAD_X_WAIT : S_CYCLE_0; // Loop in current state until go signal goes low
                 S_CYCLE_0: next_state = S_CYCLE_1;
-                S_CYCLE_1: next_state = S_LOAD_A; // we will be done our two operations, start over after
+					 S_CYCLE_1: next_state = S_CYCLE_2;
+					 S_CYCLE_2: next_state = S_CYCLE_3;
+					 S_CYCLE_3: next_state = S_CYCLE_4;
+					 S_CYCLE_4: next_state = S_CYCLE_5;
+					 S_CYCLE_5: next_state = S_LOAD_A; // we will be done our two operations, start over after
             default:     next_state = S_LOAD_A;
         endcase
     end // state_table
@@ -156,17 +164,35 @@ module control(
             S_LOAD_X: begin
                 ld_x = 1'b1;
                 end
-            S_CYCLE_0: begin // Do A <- A * A 
+            S_CYCLE_0: begin // Do A <- A * X 
                 ld_alu_out = 1'b1; ld_a = 1'b1; // store result back into A
                 alu_select_a = 2'b00; // Select register A
-                alu_select_b = 2'b00; // Also select register A
+                alu_select_b = 2'b11; // Also select registerx
                 alu_op = 1'b1; // Do multiply operation
             end
-            S_CYCLE_1: begin
-                ld_r = 1'b1; // store result in result register
+				S_CYCLE_2: begin // Do A <- AX * X 
+                ld_alu_out = 1'b1; ld_a = 1'b1; // store result back into A
                 alu_select_a = 2'b00; // Select register A
-                alu_select_b = 2'b10; // Select register C
-                alu_op = 1'b0; // Do Add operation
+                alu_select_b = 2'b11; // Also select registe rx
+                alu_op = 1'b1; // Do multiply operation
+            end
+            S_CYCLE_3: begin // Do A <- b * X 
+                ld_alu_out = 1'b1; ld_b = 1'b1; // store result back into A
+                alu_select_a = 2'b01; // Select register A
+                alu_select_b = 2'b11; // Also select registe rx
+                alu_op = 1'b1; // Do multiply operation
+            end
+				S_CYCLE_4: begin // Do A <- b + a 
+                ld_alu_out = 1'b1; ld_b = 1'b1; // store result back into A
+                alu_select_a = 2'b01; // Select register A
+                alu_select_b = 2'b00; // Also select registr b
+                alu_op = 1'b0; // Do aZZZZ operation
+            end
+				S_CYCLE_5: begin // Do R <-c + a 
+                ld_r = 1'b1;
+                alu_select_a = 2'b01; // Select register A
+                alu_select_b = 2'b10; // Also select registr b
+                alu_op = 1'b0; // Do add operation
             end
         // default:    // don't need default since we already made sure all of our outputs were assigned a value at the start of the always block
         endcase
